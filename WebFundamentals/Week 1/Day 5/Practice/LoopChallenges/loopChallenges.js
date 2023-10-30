6c653e73add7b3c91f3715161848fb6248587f05
dhiaeddine.amri <dhiaeddine.amri@gmail.com>
2023-10-30 14:01:54 +0100

    LoopChallenges

---
+++ WebFundamentals/Week 1/Day 5/Practice/LoopChallenges/loopChallenges.js
@@ -0,0 +1,28 @@
+for(i=1;i<=20;i++){
+    if(i%2===1){
+        console.log(i);
+    }
+}
+
+for(i=100;i>=0;i--){
+    if(i%3===0){
+        console.log(i);
+    }
+}
+var n=4;
+for(i=5;i>=0;i--){
+    console.log(n);
+    n-=1.5;
+}
+var sum=0;
+for(i=1;i<=100;i++){
+    sum+=i;
+}
+console.log(sum);
+
+result=1;
+
+for(i=1;i<=12;i++){
+    result*=i;
+}
+console.log(result);
