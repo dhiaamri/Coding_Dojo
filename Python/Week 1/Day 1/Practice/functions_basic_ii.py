abc31018da3868e9683063c9bce9f192804a76a7
dhiaamri <dhiaeddine.amri@gmail.com>
2023-11-16 13:03:55 +0100

    commit

---
+++ Python/Week 1/Day 1/Practice/functions_basic_ii.py
@@ -0,0 +1,68 @@
+# -----Countdown------
+
+
+# def countdown(n):
+#     numbers=[]
+#     for i in range(n,-1,-1):
+#         numbers.append(i)
+#     return numbers
+
+# print(countdown(6))
+
+
+
+
+#   Print and Return
+
+# def print_and_return(list):
+#     print(list[0])
+#     return list[1]
+
+# print(print_and_return([1,2]))
+
+
+
+# First Plus Length
+
+# def first_plus_length(numbers):
+#     total=numbers[0]+len(numbers)
+        
+#     return total
+        
+# print(first_plus_length([1,2,3,4,5]))
+        
+    
+    
+# Values Greater than Second
+        
+    
+# def values_greater_than_second(numbers):
+#     result=[]
+#     count=0
+#     if(len(numbers)<2):
+#         return false
+#     else:
+#         for i in range(0,len(numbers),1):
+#             if(numbers[i]>numbers[1]):
+#                 count+=1
+#                 result.append(numbers[i])
+#     print(count)
+#     return result
+
+# print(values_greater_than_second([5,2,3,2,1,4]))
+
+
+
+# This Length, That Value
+
+
+# def length_and_value(size,value):
+#     result=[]
+#     for i in range(0,size,1):
+#         result.append(value)
+#     return result
+
+# print(length_and_value(5,4))  
+
+
+
