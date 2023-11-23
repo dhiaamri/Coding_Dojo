88418f4d27d8063429017eca418a1b420202329d
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2023-11-23 18:24:23 +0100

    Basketball Dict

---
+++ Python/Week 1/Day 3/Practice/Basketball Dictionaries/player.py
@@ -0,0 +1,88 @@
+class Player:
+    def __init__(self, player):
+        self.name = player["name"]
+        self.age = player["age"]
+        self.position = player["position"]
+        self.team = player["team"]
+    
+
+
+kevin = {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+}
+jason = {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+}
+kyrie = {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+}
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+
+    
+# Create your Player instances here!
+Kevin = Player(kevin)
+jason = Player(jason)
+
+kyrie = Player(kyrie)
+
+
+players_list=[Player(players[3])]
+
+players_list=[Player(players[4])]
+
+players_list=[Player(players[5])]
+
+
+
+
+
+
+
