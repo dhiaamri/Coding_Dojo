4e150dad2634f30d03035bc523fe2afe8bf45eeb
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2023-12-18 19:34:59 +0100

    Hoist

---
+++ MERNStack/week_1/Day_1/Practice/hoist.js
@@ -0,0 +1,98 @@
+// console.log(hello);                                   
+// var hello = 'world';                                 
+
+// var hello  
+// console.log(hello);  
+// logs undefined   
+// hello = 'world
+
+
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+// this code will remain the same 
+// it will c.log magnet
+
+
+
+
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// this code will remain the same 
+// it will c.log only okay 
+
+
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+
+// var food = 'chicken';
+// console.log(food); logs'chicken'
+// eat();
+// function eat(){
+//     var food 
+//     food = 'half-chicken'; 
+//     console.log(food); logs 'halfchicken'
+//     food = 'gone';
+// }
+
+
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+
+// console.log(genre); logs underfind
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     var genre 
+//     genre = "rock";
+//     console.log(genre);
+//     genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre); logs 'disco'
+
+
+
+
+
+// dojo = "san jose";
+// console.log(dojo);  logs 'san jose'
+// learn();
+// function learn() {
+//     var dojo 
+//     dojo = "seattle";
+//     console.log(dojo); logs seattle 
+//     dojo = "burbank";
+//     console.log(dojo); logs burbang
+// }
+// console.log(dojo); logs san jose
+
+
