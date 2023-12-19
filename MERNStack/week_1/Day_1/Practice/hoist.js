77dc3bcb93c2dff01c98f0bcc5326c0af93dd39f
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2023-12-19 14:14:59 +0100

    predict destrcut

--- MERNStack/week_1/Day_1/Practice/hoist.js
+++ MERNStack/week_1/Day_1/Practice/hoist.js
@@ -1,98 +1,129 @@
-// console.log(hello);                                   
-// var hello = 'world';                                 
+var hello
+hello = 'world'
+console.log(hello)
 
-// var hello  
-// console.log(hello);  
-// logs undefined   
-// hello = 'world
+// var hello
+// hello = 'world'
+// console.log(hello);
 
+var needle = 'haystack'
+test()
+function test () {
+  var needle = 'magnet'
+  console.log(needle)
+}
 
-
-// var needle = 'haystack';
-// test();
 // function test(){
-//     var needle = 'magnet';
-//     console.log(needle);
-// }
+//         var needle ;
+//         needle = 'magnet';
 
-// this code will remain the same 
-// it will c.log magnet
+//         console.log(needle);
+//     }
+// var needle
+// needle = 'haystack'
+// test();
 
+var brendan = 'super cool'
+function print () {
+  brendan = 'only okay'
+  console.log(brendan)
+}
+console.log(brendan)
 
-
-
-// var brendan = 'super cool';
+// var brendan
+// brendan = 'super cool'
 // function print(){
-//     brendan = 'only okay';
-//     console.log(brendan);
-// }
-// console.log(brendan);
+//         brendan = 'only okay';
+//         console.log(brendan);
+//     }
+//     console.log(brendan);
 
-// this code will remain the same 
-// it will c.log only okay 
+var food = 'chicken'
+console.log(food)
+eat()
+function eat () {
+  food = 'half-chicken'
+  console.log(food)
+  var food = 'gone'
+}
 
-
-
-// var food = 'chicken';
-// console.log(food);
-// eat();
+// var food ;
+// food = 'chicken';
 // function eat(){
+//     var food
 //     food = 'half-chicken';
-//     console.log(food);
-//     var food = 'gone';
-// }
-
-
-// var food = 'chicken';
-// console.log(food); logs'chicken'
-// eat();
-// function eat(){
-//     var food 
-//     food = 'half-chicken'; 
 //     console.log(food); logs 'halfchicken'
 //     food = 'gone';
 // }
+// console.log(food);
+// eat();
 
+console.log(genre)
+var genre = 'disco'
+rewind()
+function rewind () {
+  genre = 'rock'
+  console.log(genre)
+  var genre = 'r&b'
+  console.log(genre)
+}
+console.log(genre)
 
-
-// console.log(genre);
-// var genre = "disco";
-// rewind();
+// var genre ;
+// genre = "disco"
 // function rewind() {
-//     genre = "rock";
-//     console.log(genre);
-//     var genre = "r&b";
-//     console.log(genre);
-// }
-// console.log(genre);
-
-
-// console.log(genre); logs underfind
-// var genre = "disco";
-// rewind();
-// function rewind() {
-//     var genre 
+//     var genre
 //     genre = "rock";
 //     console.log(genre);
 //     genre = "r&b";
 //     console.log(genre);
 // }
-// console.log(genre); logs 'disco'
+// console.log(genre);
+// rewind();
+// console.log(genre);
+
+
+
+dojo = 'san jose'
+console.log(dojo)
+learn()
+function learn () {
+  dojo = 'seattle'
+  console.log(dojo)
+  var dojo = 'burbank'
+  console.log(dojo)
+}
+console.log(dojo)
+
+
+// function learn () {
+//     var dojo
+//     dojo = 'seattle'
+//     console.log(dojo)
+//     dojo = 'burbank'
+//     console.log(dojo)
+//   }
+// dojo = 'san jose'
+// console.log(dojo)
+// learn()
+// console.log(dojo)
 
 
 
 
-
-// dojo = "san jose";
-// console.log(dojo);  logs 'san jose'
-// learn();
-// function learn() {
-//     var dojo 
-//     dojo = "seattle";
-//     console.log(dojo); logs seattle 
-//     dojo = "burbank";
-//     console.log(dojo); logs burbang
+// function makeDojo(name, students){
+//     const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
 // }
-// console.log(dojo); logs san jose
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
 
 
