fcbf3207d3cd6134f88bc42e9367cfc42d2a9821
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2023-12-21 16:36:50 +0100

    first jsx

---
+++ MERNStack/week_1/Day_3/Core/first-jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div style={{padding:"50px"}} className="App">
+      <h1 >Hello Dojo!</h1>
+      <h2>Things i need to do : </h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt everest</li>
+
+        <li>Run a marahon</li>
+        <li>feed the dogs</li>
+
+      </ul>
+
+
+    </div>
+  );
+}
+
+export default App;
