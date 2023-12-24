7b22a3749b3f2c39afde59dacdf55241c73e4709
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2023-12-24 12:21:41 +0100

    more-forms

--- MERNStack/week_1/Day_3/Practice/prop-it-up/prop-it-up/src/components/PersonCard.js
+++ MERNStack/week_1/Day_3/Practice/prop-it-up/prop-it-up/src/components/PersonCard.js
@@ -6,6 +6,7 @@ const PersonCard = (props) => {
     <h1>{props.firstName}, {props.lastName} </h1>
     <h2>age : {props.age}</h2>
     <h2>Hair Color : {props.hairColor}</h2>
+    <button>Happy birthday</button>
     </div>
   )
 }
