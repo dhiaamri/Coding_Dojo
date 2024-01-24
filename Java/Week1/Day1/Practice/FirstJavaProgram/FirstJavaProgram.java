289dfcfd1fe2a25774299a067badd5cc74e704e5
dhiaamri <108345744+dhiaamri@users.noreply.github.com>
2024-01-24 13:54:12 +0100

    CafeJava + FirstJavaProgram

---
+++ Java/Week1/Day1/Practice/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Dhia");
+        System.out.println("My age is 26");
+        System.out.println("My hometown is Bizerte");
+    }
+}
