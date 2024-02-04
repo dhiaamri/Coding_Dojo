98cbf74fd7ebd82ab6c7cc7e144cb4a1927439bd
dhiaeddine.amri <dhiaeddine.amri@gmail.com>
2024-02-04 23:29:29 +0100

    m

--- Java/Week2/Day3/Core/SaveTravels/src/main/java/com/travels/savetravels/repositories/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travels.savetravels.repositories;
-
-import com.travels.savetravels.models.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
